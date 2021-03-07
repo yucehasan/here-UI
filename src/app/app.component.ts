832017e3632dfdc2c983dd8cd6d8d4a6c2650ae2
Yüce <yuceyuce99@gmail.com>
2021-03-07 18:08:31 +0300

    Import project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketioService } from './services/socketio.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'here-UI';
+export class AppComponent{
+  username: string;
+  loggedIn: boolean;
+  constructor(private router: Router) {
+    this.username = "John Doe";
+    this.loggedIn = false;
+  }
+  goMain(){
+    this.router.navigate(['/main']);
+  }
+
+  goProfile(){
+    this.router.navigate(['/profile']);
+  }
+
+  logout(){
+    this.router.navigate(['/auth']);
+  }
 }
