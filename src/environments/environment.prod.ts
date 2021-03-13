43819c75a1abcb7233546348747972a7b10f0bb8
Yüce Hasan Kılıç <56137080+yucehasan@users.noreply.github.com>
2021-03-13 18:16:42 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,  
-  FLASK_ENDPOINT: "http://here-env.eba-8zsv9qwm.us-east-2.elasticbeanstalk.com/",
+  FLASK_ENDPOINT: "https://here-env.eba-8zsv9qwm.us-east-2.elasticbeanstalk.com/",
   RTC_ENDPOINT: "https://rtc-server-here.herokuapp.com"
 };
