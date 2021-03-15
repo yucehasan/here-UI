fb19a18673291c26e99794b084578a62f4918998
Yüce <yuceyuce99@gmail.com>
2021-03-15 18:28:40 +0300

    Minor change due to hypromerto's request

--- src/app/pages/conference/conference.component.ts
+++ src/app/pages/conference/conference.component.ts
@@ -124,7 +124,7 @@ export class ConferenceComponent implements OnInit {
         .drawImage(this.video, 0, 0, canvas.width, canvas.height);
       // convert it to a usable data URL
       //this.socketService.emitImage(canvas.toDataURL());
-      this.httpClient.post("https://hereapp-live.herokuapp.com/image", {data: canvas.toDataURL()}).subscribe(data => {
+      this.httpClient.post("https://hereapp-live.herokuapp.com/image/", {data: canvas.toDataURL()}).subscribe(data => {
         console.log(data)
       })
     }
