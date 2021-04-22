6905fe636b59dffb5b751e5b969050a20fe39255
burakokumus <burakokumus98@gmail.com>
2021-04-22 14:52:21 +0300

    Add user type to auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,29 +6,34 @@ import { Observable, of, BehaviorSubject } from 'rxjs';
 })
 export class AuthService {
 
-  authData: Object = { "token": "", "username": "" };
-
   token: string = "";
   username: string = "";
+  userType: string = "";
 
   private tokenSub = new BehaviorSubject<string>(this.token);
   private usernameSub = new BehaviorSubject<string>(this.username);
+  private userTypeSub = new BehaviorSubject<string>(this.userType);
 
   currentToken = this.tokenSub.asObservable();
   currentUsername = this.usernameSub.asObservable();
+  currentUserType = this.userTypeSub.asObservable();
 
   updateToken(newToken: string): void {
-    this.authData["token"] = newToken;
     this.token = newToken;
     this.tokenSub.next(newToken);
     console.log("Token is updated. New Token: \n" + newToken);
   }
 
   updateUsername(newUsername: string): void {
-    this.authData["username"] = newUsername;
     this.username = newUsername;
     this.usernameSub.next(newUsername);
     console.log("Username is updated. New Username: \n" + newUsername);
+  }
+
+  updateUserType(newUserType: string): void {
+    this.userType = newUserType;
+    this.userTypeSub.next(newUserType);
+    console.log("User type is updated. New User type: \n" + newUserType);
   }
 
   getToken(): Observable<string> {
@@ -37,6 +42,10 @@ export class AuthService {
 
   getUsername(): Observable<string> {
     return this.currentUsername;
+  }
+
+  getUserType(): Observable<string> {
+    return this.currentUserType;
   }
 
 }
