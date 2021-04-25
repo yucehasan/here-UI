8fe0602475b74d6c4ae6afacd6cd25d2b6422671
busrabgz <busrabgz@gmail.com>
2021-04-26 01:15:43 +0300

    Connect buttons on heading to register and login

--- src/app/components/heading/heading.component.ts
+++ src/app/components/heading/heading.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {RegisterComponent} from '../register/register.component';
+import {LoginComponent} from '../login/login.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-heading',
@@ -7,13 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeadingComponent implements OnInit {
 
+  username: string;
+  password: string;
+
   isHeading = true;
   isSubheading = true;
   isHeadingBtn = true;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+
+  openLoginDialog(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '500px',
+      data: {name: this.username, password: this.password}
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('The login dialog was closed');
+    });
+  }
+
+  openRegisterDialog(): void {
+    const dialogRef = this.dialog.open(RegisterComponent, {
+      width: '500px',
+      data: {name: this.username, password: this.password}
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('The register dialog was closed');
+    });
   }
 
 }
