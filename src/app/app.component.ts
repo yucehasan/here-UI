b85e066ec49c407213df4807a4f668eba6ec8ab2
busrabgz <busrabgz@gmail.com>
2021-04-26 15:59:15 +0300

    Seperate toolbar from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
-import { MediaMatcher } from '@angular/cdk/layout';
-import { ChangeDetectorRef, Component, OnDestroy, Inject } from '@angular/core';
-import { SE } from './directives/scroll.directive';
-import { MatDialog } from '@angular/material/dialog';
-import { DOCUMENT } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -12,47 +7,8 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.sass'],
 })
 export class AppComponent implements OnInit {
-  username: string;
-  token: string;
-  loggedIn: boolean;
-  constructor(
-    private router: Router,
-    private authService: AuthService) {
-    this.username = "";
-    this.token = "";
-    this.loggedIn = true;
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-    this.authService.getToken().subscribe(token => {
-      this.token = token;
-    })
-
-    this.authService.getUsername().subscribe(username => {
-      this.username = username;
-    })
-  }
-
-  login(username: string, token: string) {
-    this.username = username;
-    this.token = token;
-    this.loggedIn = true;
-  }
-
-  goMain() {
-    if (this.loggedIn) { this.router.navigate(['/main']); }
-    console.log(this.token);
-  }
-
-  goProfile() {
-    if (this.loggedIn) { this.router.navigate(['/profile']); }
-    console.log(this.token);
-  }
-
-  logout() {
-    if (this.loggedIn) { this.router.navigate(['/auth']); }
-    localStorage.clear();
-    this.authService.updateToken("");
-  }
-
+  ngOnInit(): void {}
+  
 }
---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,94 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy, Inject } from '@angular/core';
+import { SE } from '../../directives/scroll.directive';
+import { MatDialog } from '@angular/material/dialog';
+import { DOCUMENT } from '@angular/common';
+
+import {RegisterComponent} from '../register/register.component';
+import {LoginComponent} from '../login/login.component';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.sass']
+})
+export class ToolbarComponent implements OnDestroy {
+
+  username: string;
+  password: string;
+
+  contactFabButton: any;
+  bodyelement: any;
+  sidenavelement: any;
+
+  isActive = false;
+  isActivefadeInDown = true;
+  fixedTolbar = true;
+
+  isLoggedIn: boolean;
+
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(@Inject(DOCUMENT) document, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, public dialog: MatDialog) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.isLoggedIn = false;
+  }
+
+  public detectScroll(event: SE) {
+    
+    if (event.header) {
+      this.isActive = false;
+      this.isActivefadeInDown = true;
+      this.fixedTolbar = true;
+    }
+    
+    if (event.bottom) {
+      this.isActive = true;
+      this.isActivefadeInDown  = false;
+      this.fixedTolbar = false;
+    }
+  }
+
+  setToggleOn(){
+
+    this.bodyelement = document.getElementById('nglpage');
+    this.bodyelement.classList.add("scrollOff");
+  }
+
+  setToggleOff(){
+    
+    this.bodyelement = document.getElementById('nglpage');
+    this.bodyelement.classList.remove("scrollOff");
+  }
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  openLoginDialog(): void {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '500px',
+      data: {name: this.username, password: this.password}
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('The login dialog was closed');
+    });
+  }
+
+  openRegisterDialog(): void {
+    const dialogRef = this.dialog.open(RegisterComponent, {
+      width: '500px',
+      data: {name: this.username, password: this.password}
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('The register dialog was closed');
+    });
+  }
+
+
+}
