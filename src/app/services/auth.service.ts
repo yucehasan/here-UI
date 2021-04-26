d86732ad1d3b135f3b2e16508a50aa35fe520e6e
burakokumus <burakokumus98@gmail.com>
2021-04-26 19:37:34 +0300

    Refactor auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,15 +6,6 @@ import { Observable, of, BehaviorSubject } from 'rxjs';
 })
 export class AuthService {
 
-  token: string = "";
-  refreshToken: string = "";
-  username: string = "";
-  userType: string = "";
-
-  // private tokenSub = new BehaviorSubject<string>(this.token);
-  // private usernameSub = new BehaviorSubject<string>(this.username);
-  // private userTypeSub = new BehaviorSubject<string>(this.userType);
-
   private tokenSub = new BehaviorSubject<string>(localStorage.getItem('token'));
   private refreshTokenSub = new BehaviorSubject<string>(localStorage.getItem('refresh_token'));
   private usernameSub = new BehaviorSubject<string>(localStorage.getItem('username'));
@@ -26,28 +17,24 @@ export class AuthService {
   currentUserType = this.userTypeSub.asObservable();
 
   updateToken(newToken: string): void {
-    this.token = newToken;
     this.tokenSub.next(newToken);
     localStorage.setItem("token", newToken);
     console.log("Token is updated. New Token: \n" + newToken);
   }
 
   updateRefreshToken(newRefreshToken: string): void {
-    this.refreshToken = newRefreshToken;
     this.refreshTokenSub.next(newRefreshToken);
     localStorage.setItem("refresh_token", newRefreshToken);
     console.log("Refresh token is updated. New Token: \n" + newRefreshToken);
   }
 
   updateUsername(newUsername: string): void {
-    this.username = newUsername;
     this.usernameSub.next(newUsername);
     localStorage.setItem("username", newUsername);
     console.log("Username is updated. New Username: \n" + newUsername);
   }
 
   updateUserType(newUserType: string): void {
-    this.userType = newUserType;
     this.userTypeSub.next(newUserType);
     localStorage.setItem("userType", newUserType);
     console.log("User type is updated. New User type: \n" + newUserType);
