1747fe9089899a00e1f5e59f861a591446877608
Yüce <yuceyuce99@gmail.com>
2021-05-02 13:57:38 +0300

    Implement pdf upload UI and controller

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 import { HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { HttpService } from 'src/app/services/http.service';
+import { ErrorComponent } from '../error/error.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -21,7 +22,8 @@ export class DashboardComponent implements OnInit {
     public addCourseDialog: MatDialog,
     private router: Router,
     private authService: AuthService,
-    private httpService: HttpService
+    private httpService: HttpService,
+    private dialogController: MatDialog
   ) {
     this.token = '';
   }
@@ -37,8 +39,7 @@ export class DashboardComponent implements OnInit {
       'Bearer ' + this.token
     );
 
-    this.httpService.get(environment.BACKEND_IP + '/course', headers).
-    subscribe(
+    this.httpService.get(environment.BACKEND_IP + '/course', headers).subscribe(
       (res) => {
         console.log(res);
         this.courses = res.courses;
@@ -46,7 +47,7 @@ export class DashboardComponent implements OnInit {
       (err) => {
         console.log(err);
       }
-    )
+    );
   }
 
   addCourse(): void {
@@ -57,5 +58,48 @@ export class DashboardComponent implements OnInit {
     this.assignStudentDialog.open(AssignStudentComponent, {
       data: { courseName: courseName, courseId: courseId },
     });
+  }
+
+  uploadFile(event): void {
+    var files = (document.getElementById('pdf-input') as HTMLInputElement)
+      .files;
+    if (files.length > 0) {
+      var fileToLoad = files[0];
+      var fileReader = new FileReader();
+      var base64File;
+      fileReader.onload = (event) => {
+        base64File = event.target.result;
+        // Send base64file to backend
+
+        this.dialogController.open(ErrorComponent, {
+          data: "File successfully uploaded."
+        });
+
+      //   const headers = new HttpHeaders().set(
+      //     'Authorization',
+      //     'Bearer ' + this.token
+      //   );
+
+      //   var formData = new FormData();
+      //   formData.append('pdf', base64File);
+
+      //   this.httpService
+      //     .post(environment.BACKEND_IP + '/course', formData, headers)
+      //     .subscribe(
+      //       (res) => {
+      //         console.log(res);
+      //         this.dialogController.open(ErrorComponent, {
+      //           data: "File successfully uploaded."
+      //         });
+      //       },
+      //       (err) => {
+      //         console.log(err);
+      //       }
+      //     );
+      };
+
+      // Convert data to base64
+      fileReader.readAsDataURL(fileToLoad);
+    }
   }
 }
